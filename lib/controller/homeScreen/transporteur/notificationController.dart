2d6ade8b8c0d2c0096dcc098fc371366429e7942
ousseeema <oussama.ferchichi01@gmail.com>
2024-06-04 23:33:18 +0100

    notification page and controller

--- lib/controller/homeScreen/transporteur/notificationController.dart
+++ lib/controller/homeScreen/transporteur/notificationController.dart
@@ -1,7 +1,69 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:transmobile/api/api.dart';
+import 'package:transmobile/view/utils/appConstant.dart';
+
+import '../../../model/demande livrasion/demandelivr.dart';
 
 class TransnotificationController extends GetxController{
  
+
+ List<DemandeLiv> DemandesLivrasion = [];
+ bool Demande_Loader = false; 
+ GetAlldemandeForCurrentTrip()async{
+  Demande_Loader = true ; 
+  update();
+  try {
+    Response Demandes = await Get.find<UserApi>().GetRequest(AppConstant.Transgetalldemande);
+
+   if(Demandes.body["success"]){
+     DemandesLivrasion = [];
+     Demandes.body["data"].forEach((demande){
+      DemandesLivrasion.add(DemandeLiv.fromJson(demande));
+     });
+Future.delayed(Duration.zero, () {
+        Demande_Loader = false; 
+          update();
+        });
+        
+      Get.snackbar("Success", Demandes.body["message"],
+            backgroundColor: Colors.green, colorText: Colors.white);
+            
+
+
+   }else{
+    Future.delayed(Duration.zero, () {
+        Demande_Loader = false; 
+          update();
+        });
+        
+      Get.snackbar("Error", Demandes.body["message"],
+            backgroundColor: Colors.red, colorText: Colors.white);
+            Get.back();
+
+   }
+
+  } catch (e) {
+    Future.delayed(Duration.zero, () {
+        Demande_Loader = false; 
+          update();
+        });
+        
+      Get.snackbar("Error","Error in the Server",
+            backgroundColor: Colors.red, colorText: Colors.white);
+            Get.back();
+
+    
+  }
+
+
+
+
+
+
+ }
 
 
 }
--- lib/view/home screen/trans/notification Pages/transNotifPage.dart
+++ lib/view/home screen/trans/notification Pages/transNotifPage.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:transmobile/controller/homeScreen/transporteur/notificationController.dart';
+import 'package:transmobile/view/Client%20components/animatedtext.dart';
+import 'package:transmobile/view/utils/colors.dart';
+import 'package:transmobile/view/utils/dimenssion.dart';
 
 class TransNotificationScreen extends StatefulWidget {
   const TransNotificationScreen({super.key});
@@ -9,12 +12,73 @@ class TransNotificationScreen extends StatefulWidget {
   State<TransNotificationScreen> createState() => _TransNotificationScreenState();
 }
 
-class _TransNotificationScreenState extends State<TransNotificationScreen> {
+class _TransNotificationScreenState extends State<TransNotificationScreen> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: 2);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
     body: GetBuilder<TransnotificationController>(builder: (controller){
-      return Container();
+      return SafeArea(
+        child: Column(children: [ 
+          SizedBox(height: Dimenssions.height20,),
+          CostumeAnimatedText(text: "Notification Page"),
+          SizedBox(height: Dimenssions.height10,),
+          TabBar(
+              indicatorColor: Colors.black,
+              controller: _tabController,
+              labelColor: Colors.black,
+              unselectedLabelColor: AppColors.mainTextColor,
+              tabs: [
+                Tab(
+                  child: Row(
+                    children: [
+                      const Icon(
+                        Icons.file_open_rounded,
+                        color: AppColors.iconColor,
+                      ),
+                     
+                      CostumeAnimatedText(text: "Demandes Section")
+                    ],
+                  ),
+                ),
+                Tab(
+                  child: Row(
+                    children: [
+                      const Icon(
+                        Icons.notifications,
+                        color: AppColors.iconColor,
+                      ),
+                  
+                      CostumeAnimatedText(text: "Notifications Section")
+                    ],
+                  ),
+                ),
+                
+              ]),
+              SizedBox(
+            height: Dimenssions.height10,
+          ),
+          Expanded(
+            child: TabBarView(controller: _tabController, 
+            children: [
+               
+            
+            ]),
+          )
+
+
+      ],));
     }),
     );
   }
